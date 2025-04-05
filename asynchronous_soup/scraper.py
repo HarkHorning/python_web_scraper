0fa674e22298ec0f45d70eaf2860a7085841f900
HarkHorning <hjhorning@gmai.com>
2025-04-04 22:00:14 -0400

    saving

--- asynchronous_soup/scraper.py
+++ asynchronous_soup/scraper.py
@@ -5,6 +5,8 @@ import requests
 cookbook = [ # Add your link(s) here
     'https://www.scrapethissite.com/pages/forms/',
     'https://www.scrapethissite.com/pages/simple/'
+    # ,
+    # 'https://www.scrapethissite.com/pages/advanced/'
 ]
 
 order = 'h1' # Put what are you looking for here
@@ -12,7 +14,7 @@ order = 'h1' # Put what are you looking for here
 async def fetch(recipe):
     ingredients = requests.get(recipe)
     soup = BeautifulSoup(ingredients.text, 'html.parser')
-    your_serving = soup.find(order)
+    your_serving = soup.find(order).strip()
     # return your_serving
     print(your_serving)
 
--- simple_soup_scraper/script.py
+++ simple_soup_scraper/script.py
@@ -1,19 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 
-target = '' # Add your link here
+target = 'https://www.scrapethissite.com/pages/simple/' # Add your link here
 
 try:
     ingredients = requests.get(target)
 except Exception as err:
     print(f"Your error, sir: {err}")
 
-mark_up = 'lxml' or 'html'
+mark_up = 'lxml'
 
 le_soupe = BeautifulSoup(ingredients.text, mark_up)
 
 print(
     le_soupe.find(
         'h1'
-    ).text.strip()
+    ) # .text.strip()
 )
